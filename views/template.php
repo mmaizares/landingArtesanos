a0a2cbb66996077b27c18e5b4f96f754156aa7f4
mmaizares <mmaizaresm@gmail.com>
2019-04-18 18:34:41 -0300

    plantilla incial

---
+++ views/template.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LapsoDigital Impresion3D</title>
+   
+    <!-- Bootstrap -->
+    <link href="" rel="stylesheet">
+    
+   <!-- estilo -->
+    <link rel="stylesheet" href="css/estilos.css" type="text/css">
+    
+</head>
+
+<body>
+   <header>
+   		<!-- BARRA NAV ================
+   		==============================-->
+   		
+		<?php include "barraNav.php"; ?>
+		
+		<!-- FIN BARRA NAV ================
+   		==============================-->
+		
+	</header>
+	
+	
+  
+   <div id="body">
+  		<main>
+  			
+  		<!-- IMAGEN INICIO ================
+   		======================================-->
+  			
+   			<?php include "modules/imgInicio.php"; ?>
+   						
+   		<!-- FIN IMAGEN INICIO ================
+		==========================================-->			
+  			
+  			
+      	<!-- PRESENTACION ================
+    	==========================================-->   
+
+      		<?php include "modules/presentacion.php"; ?>
+  		
+     	<!-- FIN PRESENTACION ================
+    	==========================================--> 
+
+  				
+  		<!-- GALERIA ================
+   		======================================-->		
+  				
+		<?php include "modules/galeria.php"; ?>	
+			
+		<!-- FIN GALERIA ================
+   		======================================-->
+			
+		<!-- ICONOS OPCIONES ================
+   		======================================-->		
+  				
+		<?php include "modules/iconosOpciones.php"; ?>	
+			
+		<!-- FIN ICONOS OPCIONES ================
+   		======================================-->
+				
+		
+		<!-- FORMULARIO ================
+   		======================================-->		
+  				
+		<?php include "modules/iconosOpciones.php"; ?>	
+			
+		<!-- FIN FORMULARIO ================
+   		======================================-->		
+			
+   		</main>  	
+   </div>
+    
+   		 <!-- FOOTER ================
+   		======================================-->
+    
+    	<?php include "modules/footer.php"; ?>	
+    
+    	<!-- FIN FOOTER ================
+   		======================================-->
+
+    <!-- jQuery  -->
+    <script src=""></script>
+       
+    <!--main js-->
+    <script type="text/javascript" src=""></script>
+    
+    
+    
+</body>
+
+</html>
